5e1f026f513a43cbe929774170f5630d353c419b
dhfromkorea <dhllee424@gmail.com>
2014-06-17 01:07:27 -0700

    implemented until dragging feature. - still sucks at the collision detection

--- watchout.js
+++ watchout.js
@@ -1,123 +1,133 @@
-// start slingin' some d3 here.
-
-// basic config.
 var gameOptions = {
-  height: 450,
-  width: 700,
-  nEnemies: 30,
-  radius: 10,
-  padding: 20
+    height: 450,
+    width: 700,
+    nEnemies: 30,
+    padding: 20
 };
+
 
 var axes = {
-  x: d3.scale.linear().domain([0,1]).range([0, gameOptions.width]),
-  y: d3.scale.linear().domain([0,1]).range([0, gameOptions.height])
+  x: d3.scale.linear().domain([0,1]).range([0,gameOptions.width]),
+  y: d3.scale.linear().domain([0,1]).range([0,gameOptions.height])
 };
 
-// set up the boundaries in which players and enemies can move
-var gameBoard = d3.select(".awesomeBoard")
-            .append("svg")
-            .attr("width", gameOptions.width)
-            .attr("height", gameOptions.height);
+var board = d3.select('.board').append('svg')
+                               .attr("width", gameOptions.width)
+                               .attr("height", gameOptions.height);
 
+var scoreBoard = d3.select('.scoreboard');
 
-// create an enemy class (TBD)
-var enemiesData = d3.range(0, gameOptions.nEnemies).map(function(i){
-  return{
-    id : i,
-    cx: Math.random(),
-    cy: Math.random()
+var Enemy = function(enemyData){
+  this.enemies = board.selectAll('.enemy').data(enemyData);
+};
+
+Enemy.prototype.createEnemies = function(){
+  this.enemies.enter().append('svg:circle')
+                 .attr('class','enemy')
+                 .attr('cx', function(d){return axes.x(d.cx);})
+                 .attr('cy', function(d){return axes.y(d.cy);})
+                 .attr('r', function(d){return d.r;})
+                 .attr('fill','red');
+};
+Enemy.prototype.moveEnemies = function(){
+    this.enemies.transition()
+            .duration(1000)
+            .attr('cx', function(){return axes.x(Math.random());})
+            .attr('cy', function(){return axes.y(Math.random());})
+            .attr('r', 15)
+            .transition()
+            .duration(2000)
+            .tween('custom', tweenWithCollision);
+};
+
+var enemyData = d3.range(0, gameOptions.nEnemies).map(function(i){
+  return {
+  id : i,
+  cx : Math.random(),
+  cy : Math.random(),
+  r : 5
   };
 });
 
-var enemyCircles = gameBoard.selectAll('circle.enemy').data(enemiesData, function(d){
-  return d.id;
-});
-
-enemyCircles.enter()
-          .append('svg:circle')
-          .attr('class', 'enemy')
-          .attr('cx', function(d){ return axes.x(d.cx);})
-          .attr('cy', function(d){ return axes.y(d.cy);})
-          .attr('r',0)
-          .attr('fill', 'black');
-
-enemyCircles.transition()
-            .duration(1000)
-            .attr('r',gameOptions.radius);
-
-var moveEnemyCircles = function(){
-  enemyCircles.transition()
-            .duration(1000)
-            .attr('cx', function(){return axes.x(Math.random());})
-            .attr('cy', function(){return axes.y(Math.random());});
+var Player = function(playerData){
+  this.player = board.selectAll('.player').data(playerData);
+};
+var playerData = [{
+  cx: Math.random(),
+  cy: Math.random(),
+  r: 5
+}];
+Player.prototype.createPlayer = function(){
+  this.player.enter().append('svg:circle')
+                     .attr('class','player')
+                     .attr('fill','green')
+                     .attr('cx', function(d){return axes.x(d.cx);})
+                     .attr('cy', function(d){return axes.y(d.cy);})
+                     .attr('r', function(d){return d.r;});
+                     // .call(drag);
 };
 
-setInterval(function(){
-  return moveEnemyCircles();
-}, 2000);
-
-
-// create a Player class (TBD)
-var playerData = [{
-  x: .5,
-  y: .5,
-  width: 20,
-  height: 20
-}];
-
-var player = gameBoard.selectAll('rect.player').data(playerData);
-
-player.enter()
-      .append('svg:rect')
-      .attr('class','player')
-      .attr('x', function(d){ return axes.x(d.x);})
-      .attr('y', function(d){ return axes.y(d.y);})
-      .attr('width', function(d){return d.width;})
-      .attr('height', function(d){return d.height;})
-      .attr('fill', 'lightgreen');
-
-// click event as a trigger for drag event
 var drag = d3.behavior.drag()
-      .origin(function(d){return d;})
-      .on("drag", dragmove);
-
-function setX(x){
-  if (x  > gameOptions.width/2 - gameOptions.padding){
-    return gameOptions.width/2 - gameOptions.padding;
-  }
-  else if (x  < -(gameOptions.width/2) + gameOptions.padding){
-    return -(gameOptions.width/2) + gameOptions.padding;
-  }
-  else {
-    return x;
-  }
-}
-function setY(y){
-  if (y  > gameOptions.height/2 - gameOptions.padding){
-    return gameOptions.height/2 - gameOptions.padding;
-  }
-  else if (y  < -(gameOptions.height/2) + gameOptions.padding){
-    return -(gameOptions.height/2) + gameOptions.padding;
-  }
-  else {
-    return y;
-  }
-}
-function dragmove(d) {
-  console.log(d3.event.x);
-  console.log(gameOptions.width);
-  var x = setX(d3.event.x);
-  var y = setY(d3.event.y);
-  d3.select(this).attr("transform", "translate(" + x + "," + y + ")");
-}
-// var drag = d3.behavior.drag()
-//     .on("drag", function(){
-//       // locate the mouse pointer
-//       // initialize an array of coordinates
+                      .on('drag',function(d,i){
+                        d.cx += d3.event.dx;
+                        d.cy += d3.event.dy;
+                        d3.select(this).attr('transform',function(d,i){
+                          return 'translate(' + [d.cx,d.cy] + ')';
+                      });
+                     
+});
 
 
-//       this.transform();
-//     });
-d3.selectAll(".player").call(drag);
+var collisionCheck = function(enemy, callback, context){
+  var mousePosX = d3.mouse(this)[0];
+  var mousePosY = d3.mouse(this)[1];
+  console.log(mousePosX);
+  enemy.each(function(d,i){
+    var diffX = parseFloat(d.cx) - mousePosX;
+    var diffY = parseFloat(d.cy) - mousePosY;
+    var distance = Math.sqrt(Math.pow(diffX,2) + Math.pow(diffY,2));
+    var sumRadi = parseFloat(d.r) + player.r;
+    if (distance <= sumRadi){
+      callback.call(context);
+    }
+  });
+};
 
+var collisionCallback = function(){
+  alert('hey');
+};
+
+var tweenWithCollision = function(endData){
+  var enemy = d3.select(this);
+  var startPos = {
+    x: parseFloat(enemy.attr('cx')),
+    y: parseFloat(enemy.attr('cy'))
+  };
+  var endPos = {
+    x: axes.x(endData.x),
+    y: axes.x(endData.x)
+  };
+  return function(t){
+    var enemyNextPos;
+    collisionCheck(enemy, collisionCallback);
+    enemyNextPos = {
+      x: startPos.x + (endPos.x - startPos.x) * t,
+      y: startPos.y + (endPos.y - startPos.y) * t
+    };
+    return enemy.attr('cx', enemyNextPos.x).attr('cy', enemyNextPos.y);
+  };
+};
+
+
+
+var initialize = function(){
+  var enemy = new Enemy(enemyData);
+  enemy.createEnemies();
+  setInterval(enemy.moveEnemies.bind(enemy), 1000);
+  var player = new Player(playerData);
+  player.createPlayer();
+  // drag
+  d3.selectAll('.player').call(drag);
+};
+
+initialize();
